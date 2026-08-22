5c68753feed0a4161abbdd10dc08f9148d285514
fahmida47 <fahmida.200547@gmail.com>
2026-08-22 14:53:53 +0600

    feat: add greenhouse mock sensor data

---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,15 @@
+export const greenhouseData = {
+  temperature: 29.5,
+  humidity: 68,
+  soilMoisture: 42,
+  light: 72,
+  rain: false,
+
+  waterPump: false,
+  exhaustFan: false,
+  growLight: true,
+  roof: "OPEN",
+
+  systemStatus: "ONLINE",
+  environmentHealth: 92,
+};
